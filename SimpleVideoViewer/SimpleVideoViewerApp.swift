ca307cb155c6afd69d298cdf4b2f4a3daa97fbbe
Aleksey Kazakov <ak77m_dev@mail.ru>
2025-01-10 18:05:27 +0300

    Initial Commit

---
+++ SimpleVideoViewer/SimpleVideoViewerApp.swift
@@ -0,0 +1,32 @@
+//
+//  SimpleVideoViewerApp.swift
+//  SimpleVideoViewer
+//
+//  Created by aleksey.kazakov on 10.01.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SimpleVideoViewerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
