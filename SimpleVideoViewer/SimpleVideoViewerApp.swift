fc25ce1543edcd205e76cd06b1b6f2b1aae4d7c4
Aleksey Kazakov <ak77m_dev@mail.ru>
2025-01-30 21:54:41 +0300

    Fix before adding mobile app

--- SimpleVideoViewer/SimpleVideoViewerApp.swift
+++ SimpleVideoViewer/SimpleVideoViewerApp.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 //import SwiftData
 
+//@main
+//struct MediaGridApp: App {
+//    
+//    let manager = AppManager()
+//    
+//    var body: some Scene {
+//        WindowGroup {
+//            MainView().environmentObject(manager)
+//        }
+//    }
+//}
+
+
 @main
 struct MediaGridApp: App {
     
+    @Environment(\.scenePhase) private var scenePhase
     let manager = AppManager()
     
     var body: some Scene {
         WindowGroup {
-            MainView().environmentObject(manager)
+            MainView()
+                .environmentObject(manager)
+                .onChange(of: scenePhase) { oldPhase, newPhase in
+                    switch newPhase {
+                    case .active:
+                        manager.loadSettings()
+                        manager.startImageRefreshTimers()
+                    case .inactive, .background:
+                        manager.timers.forEach { $0.cancel() }
+                    @unknown default:
+                        break
+                    }
+                }
         }
     }
 }
